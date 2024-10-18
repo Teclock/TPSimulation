654fdc873c4846986028b482cdcd64a4c532792a
BlackWolf1204 <zoe.casteret@gmail.com>
2024-10-18 11:18:01 +0200

    init functions

---
+++ Main.py
@@ -0,0 +1,47 @@
+
+def ArriveeBus():
+
+
+def ArriveeFileC():
+
+
+def AccesControle():
+
+
+def DepartControle():  
+
+
+def ArriveeFileR():
+
+
+def AccesReparation():
+
+
+def DepartReparation():
+
+    
+
+def DepartReparation():
+
+
+
+def DebSimulation():
+    NbBus = 0
+    NbBusRep = 0
+    AireQc = 0
+    AireQr = 0
+    AireBr = 0
+    # 4
+    # 5
+
+def FinSimulation():
+    
+
+
+def MaJAires():
+
+
+
+
+DebSimulation()
+# Simulateur
