661dc4bfa906052d2d30e2a5733fbd7994a8de3d
Liam KERN <liam.kern@orange.fr>
2024-10-18 12:22:21 +0200

    fix(names) : Change names and stuff

--- Main.py
+++ Main.py
@@ -1,36 +1,36 @@
 from echeancier import Echeancier
-global NbBus, NbBusRep, AireQc, AireQr, AireBr, Qc, Qr, Bc, Br, DateSimu
+global NbBus, NbBusRep, AireQc, AireQr, AireBr, Qc, Qr, Bc, Br, DateSimu, TmpMoyenAvContr, TmpMoyenAvRep, TmpUtilCentreRep
 import random
 
 def ArriveeBus():
     global NbBus, DateSimu
 
     heure = DateSimu + random.expovariate(1/0.75)
-    echeancier.add_event("ArriveeBus", heure)
+    echeancier.add_event(ArriveeBus, heure)
     NbBus += 1
-    echeancier.add_event("ArriveeFileC", DateSimu)
+    echeancier.add_event(ArriveeFileC, DateSimu)
 
 def ArriveeFileC():
     global QC, BC, DateSimu
     QC = QC + 1
     if BC == 0:
-        echeancier.add_event("AccesControle", DateSimu)
+        echeancier.add_event(AccesControle, DateSimu)
 
 def AccesControle():
     global QC, BC, DateSimu
     QC -= 1
     BC = 1
     DateSimu += random.uniform(1 / 4, 13 / 12)
-    echeancier.add_event("DepartContrôle", DateSimu)
+    echeancier.add_event(DepartControle, DateSimu)
 
 def DepartControle():
     global QC, BC, DateSimu
     BC = 0
     if QC > 0:
-        echeancier.add_event("AccèsContrôle", DateSimu)
+        echeancier.add_event(AccesControle, DateSimu)
     reparation = random.randint(10)
     if reparation <= 3:  # 30% de chance
-        echeancier.add_event("ArrivéeFileR", DateSimu)
+        echeancier.add_event(ArriveeFileR, DateSimu)
 
 def ArriveeFileR():
     global Qr, NbBusRep, Br, DateSimu
@@ -38,7 +38,7 @@ def ArriveeFileR():
     Qr += 1
     NbBusRep += 1
     if Br < 2 :
-        echeancier.add_event("AccesReparation", DateSimu)
+        echeancier.add_event(AccesReparation, DateSimu)
 
 def AccesReparation():
     global Qr, Br, DateSimu
@@ -46,14 +46,14 @@ def AccesReparation():
     Qr -= 1
     Br += 1
     heure = DateSimu + random.uniform(2.8, 5.5)
-    echeancier.add_event("DepartReparation", heure)
+    echeancier.add_event(DepartReparation, heure)
 
 def DepartReparation():
     global Qr, Br, DateSimu
 
     Br -= 1
     if Qr > 0 :
-        echeancier.add_event("AccesReparation", DateSimu)
+        echeancier.add_event(AccesReparation, DateSimu)
 
 def DebSimulation():
     global NbBus, NbBusRep, AireQc, AireQr, AireBr, Qc, Qr, Bc, Br, DateSimu
@@ -71,13 +71,13 @@ def DebSimulation():
 
 def FinSimulation():
     echeancier.clear()
-    TmpMoyenAvContr = AireQc / NbBus # Temps d'attente moyen avant contrôle
-    TmpMoyenAvRep = AireQr / NbBusRep # Temps d'attente moyen avant réparation
-    TmpUtilCentreRep = AireBr / (2 * 160) # Taux d'utilisation du centre de réparation
+    TmpMoyenAvContr = AireQc / NbBus
+    TmpMoyenAvRep = AireQr / NbBusRep
+    TmpUtilCentreRep = AireBr / (2 * 160)
 
 def MaJAires(D1,D2):
     global AireQc, AireQr, AireBr ,Qc, Qr, Br
-    AireQC = AireQc + (D2 - D1)*Qc
+    AireQc = AireQc + (D2 - D1)*Qc
     AireQr = AireQr + (D2 - D1)*Qr
     AireBr = AireBr + (D2 - D1)*Br
 
@@ -85,8 +85,8 @@ def MaJAires(D1,D2):
 DateSimu = float(0)
 echeancier = Echeancier()
 echeancier.add_event(DebSimulation, DateSimu,10000)
-while echeancier.notvide() : #Nom à changer
-    couple = echeancier.get_next_event() #Nom à changer
+while echeancier.size() > 0 :
+    couple = echeancier.get_next_event()
     MaJAires(DateSimu,couple[1])
     DateSimu = couple[1]
     func = globals()[couple[0]]
