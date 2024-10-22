730011817a115ddad5eb04d0544073ff39492418
Saint-ouen Jimmy <jimmy.saintouen@gmail.com>
2024-10-22 18:11:12 +0200

    Ex4 début khikhi

--- Ex4.py
+++ Ex4.py
@@ -1,3 +1,7 @@
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+
 tmps_arrivee = []
 tmps_controle = []
 try:
@@ -15,3 +19,23 @@ finally:
     print(tmps_controle)
     fichier.close()
 
+# duree de controle
+n = len(tmps_controle)
+observed = tmps_controle
+expected = []
+errors = []
+
+# plt.hist(observed, bins=50)
+# plt.ylabel('Frequency')
+# plt.xlabel('Value')
+# plt.show()
+
+for _ in range(n):
+    expected.append(random.uniform(1/4, 13/12))
+
+for i in range(n):
+    errors.append(pow(observed[i] - expected[i], 2) / expected[i])
+
+error = sum(errors)
+# np.random.chisquare()
+# todo: diviser les données (obs et the) en intervalles, comparé les différences d'effectifs, khikhi, conclusion, cqfd tmtc tkt
