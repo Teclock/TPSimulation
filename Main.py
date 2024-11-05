a8924462aa9301c59af5611b0af6ab8d78faadc9
BlackWolf1204 <zoe.casteret@gmail.com>
2024-11-05 17:24:11 +0100

    Question 2 sorties simulateur

--- Main.py
+++ Main.py
@@ -76,14 +76,16 @@ def DebSimulation():
     Bc = 0
     Br = 0
     echeancier.add_event("ArriveeBus", DateSimu + random.expovariate(1 / (3 / 4)), 0)
-    echeancier.add_event("FinSimulation", 240.0, 0)
+    echeancier.add_event("FinSimulation", TempsSimulateur, 0)
 
 
 def FinSimulation():
-    global TmpMoyenAvContr, TmpMoyenAvRep, TauxUtilCentreRep
+    global TmpMoyenAvContr, TmpMoyenAvRep, TauxUtilCentreRep, TempsSimulateur, TailleMoyFileC, TailleMoyFileR
     echeancier.clear()
     TmpMoyenAvContr = AireQc / NbBus
     TmpMoyenAvRep = AireQr / NbBusRep
+    TailleMoyFileC = AireQc / TempsSimulateur
+    TailleMoyFileR = AireQr / TempsSimulateur
     TauxUtilCentreRep = AireBr / (2 * TempsSimulateur)
 
 
@@ -93,25 +95,33 @@ def MaJAires(D1, D2):
     AireQr = AireQr + (D2 - D1) * Qr
     AireBr = AireBr + (D2 - D1) * Br
 
-nbSimu = 50
+nbSimu = 1000
 
 # Fichier csv stockant les variables statistiques
 file = open("stat.csv", 'w')
-file.write("Temps simulation; 40h;;; 80h;;; 160h;;; 240h\n")
-file.write("Variable; TmpMoyenAvContr; TmpMoyenAvRep; TauxUtilCentreRep; " +
-           "TmpMoyenAvContr; TmpMoyenAvRep; TauxUtilCentreRep; " +
-           "TmpMoyenAvContr; TmpMoyenAvRep; TauxUtilCentreRep; " +
-           "TmpMoyenAvContr; TmpMoyenAvRep; TauxUtilCentreRep\n")
+file.write("Temps simulation; 40h;;;;;; 80h;;;;;; 160h;;;;;; 240h\n")
+file.write("Variable; TmpMoyenAvContr; TailleMoyFileC; TmpMoyenAvRep; TailleMoyFileR; TauxUtilCentreRep; NbBus;" +
+           "TmpMoyenAvContr; TailleMoyFileC; TmpMoyenAvRep; TailleMoyFileR; TauxUtilCentreRep; NbBus; " +
+           "TmpMoyenAvContr; TailleMoyFileC; TmpMoyenAvRep; TailleMoyFileR; TauxUtilCentreRep; NbBus; " +
+           "TmpMoyenAvContr; TailleMoyFileC; TmpMoyenAvRep; TailleMoyFileR; TauxUtilCentreRep; NbBus\n")
 
+# Résultats des simulations
 liste_TmpMoyAvContr = []
+liste_TailleMoyFileC = []
 liste_TmpMoyAvRep = []
+liste_TailleMoyFileR = []
 liste_TauxUtilCentreRep = []
+liste_NbBus = []
 for i in range(0,nbSimu):
     csvLine = "Simulation n°" + str(i + 1) + ";"
+    # Résultats pour chaque temps de simulation
+    TmpsMoyAvContr_TmpSimu = []
+    TmpsMoyAvRep_TmpSimu = []
+    TailleMoyFileC_TmpSimu = []
+    TailleMoyFileR_TmpSimu = []
+    TauxUtilCentreRep_TmpSimu = []
+    NbBus_TmpSimu = []
     for j in [40, 80, 160, 240]:
-        TmpsMoyAvContr_TmpSimu = []
-        TmpsMoyAvRep_TmpSimu = []
-        TauxUtilCentreRep_TmpSimu = []
 
         #Simulateur
         TempsSimulateur = j
@@ -126,34 +136,67 @@ for i in range(0,nbSimu):
             func()
 
         csvLine += (str(round(TmpMoyenAvContr, 3)) + "; " +
+                    str(round(TailleMoyFileC, 3)) + "; " +
                     str(round(TmpMoyenAvRep, 3)) + "; " +
-                    str(round(TauxUtilCentreRep, 3)) + "; ")
+                    str(round(TailleMoyFileR, 3)) + "; " +
+                    str(round(TauxUtilCentreRep, 3)) + "; " +
+                    str(round(NbBus, 3)) + "; ")
         TmpsMoyAvContr_TmpSimu.append(TmpMoyenAvContr)
+        TailleMoyFileC_TmpSimu.append(TailleMoyFileC)
         TmpsMoyAvRep_TmpSimu.append(TmpMoyenAvRep)
+        TailleMoyFileR_TmpSimu.append(TailleMoyFileR)
         TauxUtilCentreRep_TmpSimu.append(TauxUtilCentreRep)
+        NbBus_TmpSimu.append(NbBus)
     file.write(csvLine + "\n")
     liste_TmpMoyAvContr.append(TmpsMoyAvContr_TmpSimu)
+    liste_TailleMoyFileC.append(TailleMoyFileC_TmpSimu)
     liste_TmpMoyAvRep.append(TmpsMoyAvRep_TmpSimu)
+    liste_TailleMoyFileR.append(TailleMoyFileR_TmpSimu)
     liste_TauxUtilCentreRep.append(TauxUtilCentreRep_TmpSimu)
+    liste_NbBus.append(NbBus_TmpSimu)
     #print("Simulation #", i+1, " pour ", j, " heures")
     #print("TmpMoyenAvContr : ", TmpMoyenAvContr)
     #print("TmpMoyenAvRep : ", TmpMoyenAvRep)
     #print("TauxUtilCentreRep : ", TauxUtilCentreRep, "\n")
 
+# Calcul des esperences
 esperance_TmpMoyAvContr = []
+esperance_TailleMoyFileC = []
 esperance_TmpMoyAvRep = []
+esperance_TailleMoyFileR = []
 esperance_TauxUtilCentreRep = []
+esperance_NbBus = []
 file.write(";")
 for i in range(len(liste_TmpMoyAvContr[0])):
     esperance_TmpMoyAvContr.append(0)
+    esperance_TailleMoyFileC.append(0)
     esperance_TmpMoyAvRep.append(0)
+    esperance_TailleMoyFileR.append(0)
     esperance_TauxUtilCentreRep.append(0)
+    esperance_NbBus.append(0)
     for j in range(len(liste_TmpMoyAvContr)):
         esperance_TmpMoyAvContr[i] += liste_TmpMoyAvContr[j][i]
+        esperance_TailleMoyFileC[i] += liste_TailleMoyFileC[j][i]
         esperance_TmpMoyAvRep[i] += liste_TmpMoyAvRep[j][i]
+        esperance_TailleMoyFileR[i] += liste_TailleMoyFileR[j][i]
         esperance_TauxUtilCentreRep[i] += liste_TauxUtilCentreRep[j][i]
+        esperance_NbBus[i] += liste_NbBus[j][i]
     esperance_TmpMoyAvContr[i] /= nbSimu
+    esperance_TailleMoyFileC[i] /= nbSimu
     esperance_TmpMoyAvRep[i] /= nbSimu
+    esperance_TailleMoyFileR[i] /= nbSimu
     esperance_TauxUtilCentreRep[i] /= nbSimu
-    file.write(str(esperance_TmpMoyAvContr[i]) + ";" + str(esperance_TmpMoyAvRep[i]) + ";" + str(esperance_TauxUtilCentreRep[i]) + ";")
+    esperance_NbBus[i] /= nbSimu
+    print("esp_TmpMoyAvContr ", i, " : ", esperance_TmpMoyAvContr[i])
+    print("esp_TailleC ", i, " : ", esperance_TailleMoyFileC[i])
+    print("esp_TmpMoyAvRep ", i, " : ", esperance_TmpMoyAvRep[i])
+    print("esp_TailleR ", i, " : ", esperance_TailleMoyFileR[i])
+    print("esp_TauxUtilCentreRep ", i, " : ", esperance_TauxUtilCentreRep[i])
+    print("esp_NbBus ", i, " : ", esperance_NbBus[i], "\n")
+    file.write(str(esperance_TmpMoyAvContr[i]) + ";" + 
+                str(esperance_TailleMoyFileC[i]) + ";" + 
+                str(esperance_TmpMoyAvRep[i]) + ";" + 
+                str(esperance_TailleMoyFileR[i]) + ";" + 
+                str(esperance_TauxUtilCentreRep[i]) + ";" +
+                str(esperance_NbBus[i]) + "; ")
 file.close()
